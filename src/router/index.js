1b8dd98f8459128bce5d23a8c10c1faaa2c2aca4
TMN <2832655048@qq.com>
2025-02-27 20:59:11 +0800

    hash

--- src/router/index.js
+++ src/router/index.js
@@ -58,7 +58,7 @@ const routes = [
     },
 ]
 const router = createRouter({
-    history: createWebHistory("/Sound"),
+    history: createWebHashHistory("/Sound"),
     //history: createWebHashHistory(),
     routes: routes
 })
