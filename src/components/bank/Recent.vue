b9e15c1527b1e1a95c06fccf4b3614b17f6335c0
TMN <2832655048@qq.com>
2025-03-04 20:00:07 +0800

    创建音色界面拆分

---
+++ src/components/bank/Recent.vue
@@ -0,0 +1,82 @@
+<template>
+    <h2>最近活动</h2>
+    <div v-if="voices.length === 0">
+        <el-empty description="暂无数据" />
+    </div>
+    <el-row v-for="voice in voices" :key="voice.id" class="voice-item">
+        <el-col :span="5" style="display: flex;align-items: center;justify-content: center;">
+            <el-image style=" width: 75px; height: 75px;border-radius: 10px;" :src="voice.image" fit="cover" />
+        </el-col>
+        <el-col :span="19">
+            <div style="font-size: 18px; font-weight: 800;padding-bottom: 10px;">{{ voice.name }}</div>
+            <div class="tag">
+                <el-tag type="success" round effect="dark" style="width: 50px;font-weight: 1000;">{{
+                    voice.status
+                }}</el-tag>
+                <div style="display: flex;flex-direction: row; gap: 10px;">
+                    <el-icon size="20" style="color: #6b7280;">
+                        <Clock />
+                    </el-icon>
+                    <span style="font-weight: 520;color: #71717a;">{{ timeDistance(voice.creationTime) }}</span>
+                </div>
+            </div>
+
+        </el-col>
+    </el-row>
+</template>
+<script setup>
+import {
+    Clock,
+} from '@element-plus/icons-vue';
+import { ref } from 'vue';
+import { timeDistance } from '@/hooks/time';
+const voices = ref([
+    {
+        id: 1,
+        image: 'http://yiyangqianxihsdkhejknfnbhuyjwes.online/975adcd7-15bf-44d4-a440-be2fbc972af1.jpg',
+        name: '55',
+        description: '1212',
+        creationTime: new Date(2025, 1, 9, 19, 11),
+        status: "成功",
+        audioUrl: '/samples/sample1.mp3'
+    },
+    {
+        id: 2,
+        image: 'http://yiyangqianxihsdkhejknfnbhuyjwes.online/975adcd7-15bf-44d4-a440-be2fbc972af1.jpg',
+        name: '55',
+        description: '1212',
+        creationTime: new Date(2025, 1, 9, 19, 11),
+        status: "成功",
+        audioUrl: '/samples/sample1.mp3'
+    },
+])
+</script>
+<style scoped>
+.reactivity {
+    width: 100%;
+    padding-left: 3%;
+}
+
+.voice-item {
+    background-color: white;
+    border-radius: 10px;
+    border: #e4e4e7 1px solid;
+    display: flex;
+    padding: 15px;
+    margin-bottom: 20px;
+
+    .tag {
+        padding-top: 3%;
+        display: flex;
+        flex-direction: row;
+        gap: 10px;
+        align-items: center;
+    }
+
+    .el-col {
+        display: flex;
+        flex-direction: column;
+        gap: 1px;
+    }
+}
+</style>
--- src/views/bank/CreateSound.vue
+++ src/views/bank/CreateSound.vue
@@ -173,35 +173,13 @@
         </el-col>
         <el-divider direction="vertical" style="height: auto;" />
         <el-col :span="11" class="reactivity">
-            <h2>最近活动</h2>
-            <div v-if="voices.length === 0">
-                <el-empty description="暂无数据" />
-            </div>
-            <el-row v-for="voice in voices" :key="voice.id" class="voice-item">
-                <el-col :span="5" style="display: flex;align-items: center;justify-content: center;">
-                    <el-image style=" width: 75px; height: 75px;border-radius: 10px;" :src="voice.image" fit="cover" />
-                </el-col>
-                <el-col :span="19">
-                    <div style="font-size: 18px; font-weight: 800;padding-bottom: 10px;">{{ voice.name }}</div>
-                    <div class="tag">
-                        <el-tag type="success" round effect="dark" style="width: 50px;font-weight: 1000;">{{
-                            voice.status
-                            }}</el-tag>
-                        <div style="display: flex;flex-direction: row; gap: 10px;">
-                            <el-icon size="20" style="color: #6b7280;">
-                                <Clock />
-                            </el-icon>
-                            <span style="font-weight: 520;color: #71717a;">{{ timeDistance(voice.creationTime) }}</span>
-                        </div>
-                    </div>
-
-                </el-col>
-            </el-row>
+            <Recent/>
         </el-col>
     </el-row>
 </template>
 
 <script setup>
+import Recent from "@/components/bank/Recent.vue";
 import MyInputW from "@/components/newComponent/Input.vue";
 import AudioPlayer from "@/components/newComponent/AudioPlayer.vue";
 const samples = reactive([]);
@@ -241,28 +219,6 @@ function handleMessageName2(index, newMessage) {
 function handleMessageTextArea(index, newMessage) {
     samples[index].text = newMessage;
 }
-import { timeDistance } from '@/hooks/time';
-// 模拟数据
-const voices = ref([
-    {
-        id: 1,
-        image: 'http://yiyangqianxihsdkhejknfnbhuyjwes.online/975adcd7-15bf-44d4-a440-be2fbc972af1.jpg',
-        name: '55',
-        description: '1212',
-        creationTime: new Date(2025, 1, 9, 19, 11),
-        status: "成功",
-        audioUrl: '/samples/sample1.mp3'
-    },
-    {
-        id: 2,
-        image: 'http://yiyangqianxihsdkhejknfnbhuyjwes.online/975adcd7-15bf-44d4-a440-be2fbc972af1.jpg',
-        name: '55',
-        description: '1212',
-        creationTime: new Date(2025, 1, 9, 19, 11),
-        status: "成功",
-        audioUrl: '/samples/sample1.mp3'
-    },
-])
 const files = ref([]);
 const recording = ref(false);
 const mediaRecorder = ref(null);
@@ -414,7 +370,6 @@ import {
     CircleCheck,
     CircleClose,
     Close,
-    Clock,
 } from '@element-plus/icons-vue';
 import Step from '@/components/bank/Step.vue';
 import MyInput from '@/components/newComponent/Input.vue';
@@ -737,33 +692,7 @@ const uploadSuccess = (result) => {
     cursor: pointer;
 }
 
-.voice-item {
-    background-color: white;
-    border-radius: 10px;
-    border: #e4e4e7 1px solid;
-    display: flex;
-    padding: 15px;
-    margin-bottom: 20px;
 
-    .tag {
-        padding-top: 3%;
-        display: flex;
-        flex-direction: row;
-        gap: 10px;
-        align-items: center;
-    }
-
-    .el-col {
-        display: flex;
-        flex-direction: column;
-        gap: 1px;
-    }
-}
-
-.reactivity {
-    width: 100%;
-    padding-left: 3%;
-}
 
 .audio-container {
     width: 100%;
