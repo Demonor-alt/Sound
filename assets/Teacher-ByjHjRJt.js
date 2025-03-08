0ce371e139a20fb45abcb1ac33528fa8cbfcaef5
Demonor-alt <Demonor-alt@users.noreply.github.com>
2025-03-08 00:50:30 +0000

    deploy: 5102939866c397d40b5de6bfad1b534f4e4f87ed

---
+++ assets/Teacher-ByjHjRJt.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-D4eGm4ta.js";const r={};function c(t,_){return"4"}const o=e(r,[["render",c]]);export{o as default};
---
+++ assets/token-DawUwK3w.js
@@ -0,0 +1,1 @@
+import{R as t,r as n}from"./index-D4eGm4ta.js";const u=t("token",()=>{const e=n("");return{token:e,setToken:o=>{e.value=o},removeToken:()=>{e.value=""}}},{persist:!0});export{u};
