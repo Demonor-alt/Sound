f486fa2fd56bbcf29c394c5a31607e381d1ae57c
Demonor-alt <Demonor-alt@users.noreply.github.com>
2025-03-12 12:00:44 +0000

    deploy: 588021b36555e30ad8d02fc815e20cc8f1b3415f

--- assets/teach-CSUpJiG5.js
+++
@@ -1,1 +0,0 @@
-import{i as r}from"./request-gQz7j1j0.js";const c=e=>r.get(`/teach/pronounce/${e}`),a=e=>r.post("/teach/record",e),o=e=>r.get("/teach/exercise",{params:e});export{o as a,a as c,c as t};
