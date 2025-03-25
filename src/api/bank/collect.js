ff3c094096a093489698e4a7a3acfe0ba786cead
TMN <2832655048@qq.com>
2025-03-25 19:13:11 +0800

    修改接口

--- src/api/bank/collect.js
+++ src/api/bank/collect.js
@@ -5,8 +5,8 @@ export const collectQueryService = () => {
     return request.get(`/collect/all`);
 }
 //批量删除
-export const collectBatchDeleteService = (params) => {
-    return request.delete('/collect',{ params: params })
+export const collectBatchDeleteService = (data) => {
+    return request.put('/collect',data)
 }
 //查询收藏音色所有信息
 export const collectQueryAllService = () => {
