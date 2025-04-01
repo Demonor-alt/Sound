5079a3fd3332dca4693ab51a2c3c57e6ccf01a3f
Demonor-alt <Demonor-alt@users.noreply.github.com>
2025-04-01 00:40:16 +0000

    deploy: 15547de3b2feb0130388bdb545a72afd875e1757

---
+++ assets/token-DeMosg4p.js
@@ -0,0 +1,1 @@
+import{ac as t,r as n}from"./index-LwWqLb6a.js";const a=t("token",()=>{const e=n("");return{token:e,setToken:o=>{e.value=o},removeToken:()=>{e.value=""}}},{persist:!0});export{a as u};
