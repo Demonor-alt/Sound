4bf0d3ddbabc178d9c2235d9d33a371f34a7ffed
TMN <2832655048@qq.com>
2025-04-09 21:09:25 +0800

    虚拟教师白板展示

--- src/components/MainPanel/board/blank.vue
+++ src/components/MainPanel/board/blank.vue
@@ -1,12 +1,12 @@
 <script setup></script>
 <template>
     <div class="flex justify-center items-center h-1/3 mt-4">
-        <img src="/icon.png" class="rounded-full h-full" alt="blank" />
+        <img src="../../../assets/logo.png" class="rounded-full h-full" alt="blank" />
     </div>
     <div class="text-center lxgwfont flex-wrap">
-        <p class="lxgwfont-bold my-4 text-6xl">璇玑智灵</p>
-        <p class="lxgwfont-bold text-4xl my-2">EvoIndustry</p>
-        <p class="lxgwfont-bold text-2xl my-2">欢迎使用璇玑智灵，一款工业生产智能数字人机交互系统。</p>
+        <p class="lxgwfont-bold my-4 text-6xl">声之型</p>
+        <p class="lxgwfont-bold text-4xl my-2">THE SHAPE OF SOUND</p>
+        <p class="lxgwfont-bold text-2xl my-2">欢迎使用声之型，一款数字人机语音交互系统。</p>
         <p class="lxgwfont-bold text-2xl mt-20">请在下方文本输入框中输入所问的问题，或者直接点击“麦克风”图标语音输入。</p>
     </div>
 
