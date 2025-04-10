37df507ec37822edb7b2c30d644bea6a04d38353
TMN <2832655048@qq.com>
2025-04-10 08:38:06 +0800

    虚拟教师白板展示

--- src/components/MainPanel/board/blank.vue
+++ src/components/MainPanel/board/blank.vue
@@ -6,7 +6,8 @@
     <div class="text-center lxgwfont flex-wrap">
         <p class="lxgwfont-bold my-4 text-6xl">声之型</p>
         <p class="lxgwfont-bold text-4xl my-2">THE SHAPE OF SOUND</p>
-        <p class="lxgwfont-bold text-2xl my-2">欢迎使用声之型，一款数字人机语音交互系统。</p>
+        <!-- <p class="lxgwfont-bold text-2xl my-2">欢迎使用声之型，一款数字人机语音交互系统。</p> -->
+        <p class="lxgwfont-bold text-2xl my-2">欢迎使用声之型，一款基于GPT-SoVITS的高自然度AI语音生成教学软件。</p>
         <p class="lxgwfont-bold text-2xl mt-20">请在下方文本输入框中输入所问的问题，或者直接点击“麦克风”图标语音输入。</p>
     </div>
 
